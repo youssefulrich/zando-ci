5dc35fe13eda1ba5713fbc30a2b771fadcdaf034
Ulrich Koffi <koffiulrich538@gmail.com>
2026-04-24 10:41:51 +0000

    fixe: middleware update

--- middleware.ts
+++ middleware.ts
@@ -58,5 +58,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|robots.txt|og.png|og.jpg|og.jpeg|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
